9a3a48d289aa116e5f4f7ef38da386339fdbfbd7
Rudra <merudramanu@gmail.com>
2023-04-21 00:08:31 +0530

    added content

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
-const port = 9500;
+const middlewares = jsonServer.defaults();
 
+server.use(middlewares);
+// server.use((req, res, next) => {
+//   if (isAuthorized(req)) {
+//     // add your authorization logic here
+//     next(); // continue to JSON Server router
+//   } else {
+//     res.sendStatus(401);
+//   }
+// });
 server.use(router);
-server.listen(port);
+server.listen(3000, () => {
+  console.log("JSON Server is running");
+});
